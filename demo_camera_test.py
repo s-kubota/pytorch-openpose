eee9604bdea678f941f9b679b7f5796621c5b261
Seiya Kubota <seiya.kubota@setsunan.ac.jp>
2023-07-16 15:26:32 +0900

    ADD no_hands arg

--- demo_camera_test.py
+++ demo_camera_test.py
@@ -9,8 +9,16 @@ from src import util
 from src.body import Body
 from src.hand import Hand
 
+import argparse
+
 body_estimation = Body('model/body_pose_model.pth')
 hand_estimation = Hand('model/hand_pose_model.pth')
+
+parser = argparse.ArgumentParser(
+        description="Process a camera annotating poses detected.")
+parser.add_argument('--no_hands', action='store_true', help='No hand pose')
+args = parser.parse_args()
+is_hands = not args.no_hands
 
 print(f"Torch device: {torch.cuda.get_device_name()}")
 
@@ -24,16 +32,17 @@ while True:
     canvas = util.draw_bodypose(canvas, candidate, subset)
 
     # detect hand
-    hands_list = util.handDetect(candidate, subset, oriImg)
+    if is_hands:
+        hands_list = util.handDetect(candidate, subset, oriImg)
 
-    all_hand_peaks = []
-    for x, y, w, is_left in hands_list:
-        peaks = hand_estimation(oriImg[y:y+w, x:x+w, :])
-        peaks[:, 0] = np.where(peaks[:, 0]==0, peaks[:, 0], peaks[:, 0]+x)
-        peaks[:, 1] = np.where(peaks[:, 1]==0, peaks[:, 1], peaks[:, 1]+y)
-        all_hand_peaks.append(peaks)
+        all_hand_peaks = []
+        for x, y, w, is_left in hands_list:
+            peaks = hand_estimation(oriImg[y:y+w, x:x+w, :])
+            peaks[:, 0] = np.where(peaks[:, 0]==0, peaks[:, 0], peaks[:, 0]+x)
+            peaks[:, 1] = np.where(peaks[:, 1]==0, peaks[:, 1], peaks[:, 1]+y)
+            all_hand_peaks.append(peaks)
 
-    canvas = util.draw_handpose(canvas, all_hand_peaks)
+        canvas = util.draw_handpose(canvas, all_hand_peaks)
 
     cv2.imshow('demo', canvas)
     if cv2.waitKey(1) & 0xFF == ord('q'):
